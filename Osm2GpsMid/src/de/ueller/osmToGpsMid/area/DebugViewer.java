63b1b2ce77017afd4694f6da634b55ef8a94ef25
Martin Tschöpe <martin.tschoepe@web.de>
2013-05-26 00:24:53 +0200

    added some features to the debugviewer

--- Osm2GpsMid/src/de/ueller/osmToGpsMid/area/DebugViewer.java
+++ Osm2GpsMid/src/de/ueller/osmToGpsMid/area/DebugViewer.java
@@ -13,10 +13,14 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Polygon;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
 
 import javax.swing.JFrame;
 import javax.swing.event.MouseInputListener;
@@ -32,6 +36,8 @@ import de.ueller.osmToGpsMid.model.Node;
 public class DebugViewer
 		extends JFrame {
 	private static final long serialVersionUID = -2785651417345285948L;
+
+	private static final boolean WAIT_FOR_CLICK = false;
 	
 	private static DebugViewer instance = null;
 	
@@ -53,9 +59,7 @@ public class DebugViewer
 	private Outline activeOutline = null;
 	private Triangle currentTriangle = null;
 	private Vertex currentInsideVertex = null;
-	
-	private boolean computing = true;
-	
+	private volatile BufferedImage backBuffer = null;
 	
 	
 	private final MouseInputListener mouseListener = new MouseInputListener() {
@@ -78,6 +82,7 @@ public class DebugViewer
 		@Override
 		public void mouseClicked(MouseEvent e) {
 			// Ignore
+			clickSemaphore.release();
 		}
 
 		@Override
@@ -113,6 +118,8 @@ public class DebugViewer
 			}
 		}
 	};
+
+	private final Semaphore clickSemaphore = new Semaphore(1);
 	
 	/**
 	 * 
@@ -125,6 +132,27 @@ public class DebugViewer
 		getContentPane().addMouseListener(mouseListener);
 		getContentPane().addMouseMotionListener(mouseListener);
 		getContentPane().addMouseWheelListener(mouseWheelListener);
+		KeyListener keyListener = new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				clickSemaphore.release();
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+		getContentPane().addKeyListener(keyListener);
+  		addKeyListener(keyListener);
 	}
 
 	@Override
@@ -134,27 +162,17 @@ public class DebugViewer
 			xs = getSize().height;
 			recalcView();
 		}
+		if (backBuffer == null) {
+			recreateBackbuffer();
+		}
 		Graphics2D g2=(Graphics2D) g;
-		g2.setBackground(Color.WHITE);
-		g.clearRect(0, 0, getWidth(), getHeight());
+		synchronized (this) {
+			g2.drawImage(backBuffer, 0, 0, null);
+		}
 		try {
-			g2.setColor(Color.DARK_GRAY);
-			for (Outline o : a.getOutlineList()){
-				drawOutline(g2, o,0);
-			}
-			for (Outline o : a.getHoleList()){
-				drawOutline(g2, o,0);
-			}
 			g2.setColor(Color.cyan);
 			if (activeOutline != null) {
 				drawOutline(g2, activeOutline,0);
-			}
-			Color cf = new Color(0,255,0,50);		
-			Color co = Color.BLACK;
-			if (a.triangleList != null) {
-				for (Triangle t : a.triangleList){
-					drawTriangle(g2, t, cf, co);
-				}
 			}
 			if (currentTriangle != null){
 				drawTriangle(g2, currentTriangle, new Color(255,0,0,40), Color.RED);
@@ -162,19 +180,47 @@ public class DebugViewer
 			Color cAlt = new Color(255,255,0,40);
 			if (alt != null){
 				for (Triangle t:alt) {
-					drawTriangle(g2, t, cAlt, co);
+					drawTriangle(g2, t, cAlt, Color.BLACK);
 				}
 			}
 			if (currentInsideVertex != null){
-				Point ei=toScreen(currentInsideVertex.getNode());
+				Point ei = toScreen(currentInsideVertex.getNode());
 				g2.setColor(Color.magenta);
-				g2.drawString("*", ei.x, ei.y);
+				g2.fillOval(ei.x - 5, ei.y - 5, 10, 10);
 			}
 		} catch (Exception e) {
 			System.out.println("error while painting " + e.getLocalizedMessage());
 			e.printStackTrace();
 		}
 	}
+	/**
+	 * 
+	 */
+	private synchronized void recreateBackbuffer() {
+		BufferedImage backBuffer_ = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+		Graphics2D g2 = (Graphics2D) backBuffer_.getGraphics();
+		backBuffer = backBuffer_;
+		g2.setBackground(Color.WHITE);
+		g2.clearRect(0, 0, getWidth(), getHeight());
+		
+		g2.setColor(Color.DARK_GRAY);
+		if (activeOutline != null) {
+			drawOutline(g2, activeOutline,0);
+		}
+		for (Outline o : a.getOutlineList()){
+			drawOutline(g2, o,0);
+		}
+		for (Outline o : a.getHoleList()){
+			drawOutline(g2, o,0);
+		}
+		if (a.triangleList != null) {
+			for (Triangle t : a.triangleList){
+				drawTriangle(t, g2);
+			}
+		}
+		
+	}
+
 	/**
 	 * @param g2
 	 * @param t
@@ -185,10 +231,10 @@ public class DebugViewer
 		g2.setColor(cf);
 		Point p;
 		Vertex[] vert = t.getVert();
-		Polygon po=new Polygon();
-		for (int i=0;i<3;i++){
-			p=toScreen(vert[i].getNode());
-			po.addPoint(p.x,p.y);
+		Polygon po = new Polygon();
+		for (int i = 0; i < 3; i++){
+			p = toScreen(vert[i].getNode());
+			po.addPoint(p.x, p.y);
 		}
 		g2.fillPolygon(po);
 		g2.setColor(co);
@@ -211,9 +257,9 @@ public class DebugViewer
 			n=v.getNode();
 			Point e=toScreen(n);
 			if (s != null){
-				g2.drawLine(s.x+xoff, s.y, e.x+xoff, e.y);
+				g2.drawLine(s.x + xoff, s.y, e.x + xoff, e.y);
 				if (xoff != 0){
-					g2.drawString(""+i++, s.x+xoff+3, s.y-3);
+					g2.drawString("" + i++, s.x + xoff + 3, s.y - 3);
 				}
 			}
 			s=e;
@@ -235,8 +281,7 @@ public class DebugViewer
 	 * @param a the a to set
 	 */
 	public void setArea(Area a) {
-		this.a=a;
-		computing = true;
+		this.a = a;
 		recalcView();
 	}
 
@@ -256,21 +301,20 @@ public class DebugViewer
 		}
 		offsetY = b.minLat;
 		offsetX = b.minLon;
+		backBuffer = null;
 	}
 
 	protected void zoom(float g) {
 		zoomFactor *= g;
-		if (! computing) {
-			repaint();
-		}
+		backBuffer = null;
+		repaint();
 	}
 	
 	protected void pan(int dx, int dy) {
 		offsetX -= dx / zoomFactor;
 		offsetY += dy / zoomFactor;
-		if (! computing) {
-			repaint();
-		}
+		backBuffer = null;
+		repaint();
 	}
 
 	public void setActiveOutline(Outline outline) {
@@ -282,8 +326,43 @@ public class DebugViewer
 		currentInsideVertex = vertexInside;
 	}
 
-	public void done() {
-		computing  = false;
+	private void drawTriangle(Triangle t, Graphics2D g2) {
+		drawTriangle(g2, t, new Color(0,255,0,50), Color.BLACK);
 	}
 	
+	public synchronized void triangleAdded(Triangle t) {
+		if (backBuffer != null) {
+			drawTriangle(t, backBuffer.createGraphics());
+		}
+	}
+	
+
+
+	public void done() {
+	}
+
+	public void waitToContinue() {
+		try {
+			if (WAIT_FOR_CLICK) {
+				clickSemaphore.acquire();
+			} else {
+					Thread.sleep(10);
+			}
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			throw new RuntimeException(e);
+		}
+	}
+
+	/**
+	 * @param newOutline
+	 */
+	public synchronized void addOutline(Outline newOutline) {
+		if (backBuffer != null) {
+			Graphics2D g2 = backBuffer.createGraphics();
+			g2.setColor(Color.DARK_GRAY);
+			drawOutline(g2, newOutline, 0);
+		}
+	}
+
 }
